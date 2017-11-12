3ba22f5943babd97b18ac0327e6094c25dda1794
renanbrando <reebrando@gmail.com>
2017-11-11 23:59:30 -0200

    Added first exercise.

---
+++ MainPage/MainPagePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MainPage
+{
+    public partial class MainPagePage : ContentPage
+    {
+        SettingsPage sp;
+
+        public MainPagePage()
+        {
+            InitializeComponent();
+            sp = new SettingsPage();
+        }
+
+        private void btnSend_Clicked(object sender, EventArgs e){
+            
+            if (sp.Receive){
+                DisplayAlert("Message", string.Format("E-mail sent successfully to {0}", sp.Mail), "OK");
+            } else {
+                DisplayAlert("Message", "E-mail will not be send.", "OK");
+            }
+        }
+
+        private void btnSettings_Clicked(object sender, EventArgs e){
+
+            Navigation.PushAsync(sp);
+        }
+
+
+    }
+}
---
+++ MainPage/SettingsPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MainPage
+{
+    public partial class SettingsPage : ContentPage
+    {
+        bool receive;
+        String mail;
+
+        public SettingsPage()
+        {
+            InitializeComponent();
+            receive = swCell.On;
+        }
+
+        public bool Receive{
+            get{
+                return receive;
+            }
+
+            set{
+                receive = value;
+            }
+        }
+
+        public String Mail
+        {
+            get
+            {
+                return mail;
+            }
+
+            set
+            {
+                mail = value;
+            }
+        }
+
+        public void swReceive_Switched(object sender, EventArgs e){
+            if (swCell.On){
+                receive = true;
+                txtMail.IsEnabled = true;
+            } else {
+                receive = false;
+                txtMail.IsEnabled = false;
+            }
+
+        }
+
+        public void txtName_Completed(object sender, EventArgs e){
+            mail = txtMail.Text;
+        }
+    }
+}
